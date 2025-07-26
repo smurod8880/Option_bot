9bd009ca237453c8d90ff8455f27cfb24dfeffb3
smurod8880 <squotextrade@gmail.com>
2025-07-27 01:55:49 +0500

    Update risk_manager.py

--- risk_manager.py
+++ risk_manager.py
@@ -1,28 +1,33 @@
 from datetime import datetime, timedelta
 
-class RiskManager:
+class AdvancedRiskManager:
     def __init__(self):
         self.signal_counts = {'minute': 0, 'hour': 0, 'day': 0}
         self.last_signal_time = datetime.now()
         
     def validate_signal(self, signal):
-        # Проверка лимитов
         current_time = datetime.now()
         
-        # Сброс счетчиков
+        # Сброс счетчиков при смене периода
         if current_time.minute != self.last_signal_time.minute:
             self.signal_counts['minute'] = 0
-            
         if current_time.hour != self.last_signal_time.hour:
             self.signal_counts['hour'] = 0
-            
         if current_time.day != self.last_signal_time.day:
             self.signal_counts['day'] = 0
         
-        # Проверка лимитов
+        # Проверка лимитов (5/час, 35/день)
         if (self.signal_counts['minute'] >= 2 or 
-            self.signal_counts['hour'] >= 10 or 
-            self.signal_counts['day'] >= 50):
+            self.signal_counts['hour'] >= 5 or 
+            self.signal_counts['day'] >= 35):
+            return False
+            
+        # Минимальная точность 75%
+        if signal['confidence'] < 75.0:
+            return False
+            
+        # Минимальный интервал 60 секунд
+        if (current_time - self.last_signal_time).total_seconds() < 60:
             return False
             
         return True
